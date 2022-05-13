98b24142b0ffb25cb39023fb7af08c7051c37a4a
jayshree430 <iyer430@gmail.com>
2022-05-13 23:23:45 +0100

    Created Ingredients, MealIngredientAs classes

---
+++ src/main/java/ateam/techreturners/healthyfood/model/Ingredients.java
@@ -0,0 +1,43 @@
+package ateam.techreturners.healthyfood.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ingredients {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    int calories;
+
+    @Column
+    int carbohydrates;
+
+    @Column
+    int fat;
+
+    @Column
+    int protein;
+
+    @Column
+    String blob;
+
+}
